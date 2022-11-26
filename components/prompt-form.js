41471ad7046156546b443e2e24653028ba8dd57b
Pbmangan <42583504+Pbmangan@users.noreply.github.com>
2022-11-26 12:39:19 -0500

    update

--- components/prompt-form.js
+++ components/prompt-form.js
@@ -35,7 +35,7 @@ export default function PromptForm(props) {
         />
 
         <button
-          className="bg-black text-white rounded-r-md text-small inline-block px-3 flex-none"
+          className="bg-black text-white rounded-md py-2 text-small inline-block px-3 flex-none"
           type="submit"
           onClick={() => setLoading(true)}
         >
