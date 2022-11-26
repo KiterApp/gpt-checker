b2df5e5aec99091429ebcdefde2c17b27deb3ae3
Pbmangan <42583504+Pbmangan@users.noreply.github.com>
2022-11-26 11:12:35 -0500

    Update

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="max-w-[512px] mx-auto p-10 bg-white rounded-lg">
       <Head>
-        <title>Inpainting with Stable Diffusion &amp; Replicate</title>
+        <title>Peter's Dreambooth Studio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       {/* <h1 className="text-center text-7xl pb-3">🎨</h1> */}
