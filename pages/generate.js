81d910502f591c872cbe52922cd06f9d28519cba
Pbmangan <42583504+Pbmangan@users.noreply.github.com>
2022-11-26 12:46:27 -0500

    Loading

--- pages/generate.js
+++ pages/generate.js
@@ -118,10 +118,10 @@ export default function Home() {
         </div>
         {/* Loading  */}
         {loading && (
-          <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900">
+          <div className="flex justify-center items-center h-screen text-sm text-gray-500">
+            <p>
               Loading....  (takes around 90 seconds)
-            </div>
+            </p>
           </div>
         )}
       </main>
